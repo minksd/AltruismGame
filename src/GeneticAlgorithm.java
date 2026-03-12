702a8ac6160a9cb684f7880d51938f427679283a
Daniel Minks <danielminks1230@gmail.com>
2026-03-11 21:28:46 -0400

    I think the game works?

--- src/GeneticAlgorithm.java
+++ src/GeneticAlgorithm.java
@@ -1,15 +1,39 @@
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class GeneticAlgorithm {
-    private int numGenerations;
-    private int generationSize;
-    private int mutationRate;
-    private int numAgents;
+    private int numGenerations = 10;
+    private int generationSize = 5;
+    private int mutationRate = 1;
+    private int numAgents = 10;
+    private int numTicks = 1000;
     private List<Agent> agents;
 
-    public static void main(String[] args){
-    }
+	public static void main(String[] args) {
+		GeneticAlgorithm ga = new GeneticAlgorithm();
+		Agent[] agents = new Agent[ga.numAgents];
 
+		for (int agentNum = 0; agentNum < ga.numAgents; agentNum++) {
+		    Choice[] choices = (new java.util.Random())
+			.doubles(ga.numTicks)
+			.mapToObj(ga::getChoiceFromInt)
+			.collect(Collectors.toList()).toArray(new Choice[ga.numTicks]);
+		    
+		    Agent agent = new Agent(choices);
+		    agents[agentNum] = agent;
+		}
+		Game game = new Game(agents, ga.numTicks);
+		game.runGame();
+	}
+
+	public Choice getChoiceFromInt(double choice) {
+	    if (Math.round(choice) == 0) {
+			return Choice.War;
+		} else {
+			return Choice.Peace;
+		}
+	}
     private void createAgents(){}
     private void buildGame(){}
     private void runGame(){}
